d14249c3d5096be9648f854aad83edc38c07af23
chenmengrui <chenmengrui@kp99.cn>
2019-11-11 11:17:06 +0800

    swagger2.0

--- src/main/java/com/cmr/datasource/config/SwaggerConfig.java
+++ src/main/java/com/cmr/datasource/config/SwaggerConfig.java
@@ -1,11 +1,16 @@
 package com.cmr.datasource.config;
 
 import com.fasterxml.classmate.TypeResolver;
+import com.google.common.base.Predicates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
@@ -24,7 +29,20 @@ public class SwaggerConfig {
 
     @Bean
     public Docket petApi() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .paths(Predicates.not(PathSelectors.regex("/actuator.*")))
+                .paths(Predicates.not(PathSelectors.regex("/profile.*")))
+                .paths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    protected ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("datasource").build();
     }
 
 }
--- src/main/java/com/cmr/datasource/filter/DataSourceConnectionLogFilter.java
+++ src/main/java/com/cmr/datasource/filter/DataSourceConnectionLogFilter.java
@@ -3,9 +3,12 @@ package com.cmr.datasource.filter;
 import com.alibaba.druid.filter.FilterChain;
 import com.alibaba.druid.filter.FilterEventAdapter;
 import com.alibaba.druid.proxy.jdbc.ConnectionProxy;
+import com.alibaba.druid.proxy.jdbc.PreparedStatementProxy;
+import com.alibaba.druid.proxy.jdbc.StatementProxy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLException;
 import java.util.Properties;
 
 /**
@@ -25,5 +28,11 @@ public class DataSourceConnectionLogFilter extends FilterEventAdapter {
     @Override
     public void connection_connectAfter(ConnectionProxy connection) {
         log.info("DataSourceConnectionLogFilter#connection_connectAfter");
+    }
+
+    @Override
+    public PreparedStatementProxy connection_prepareStatement(FilterChain chain, ConnectionProxy connection, String sql) throws SQLException {
+        log.info(sql);
+        return super.connection_prepareStatement(chain, connection, sql);
     }
 }
